0dcbd16e46bb2913a6091496526cf730dff790f2
Prathiksha2022 <113023616+Prathiksha2022@users.noreply.github.com>
2023-05-09 19:44:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,43 @@
-from flask import Flask, request, jsonify, render_template
 import pickle
-from nltk.corpus import stopwords
+from flask import Flask, request, jsonify
 import re
-from nltk.stem.porter import PorterStemmer
-import nltk
-nltk.download('stopwords')
+from nltk import PorterStemmer
+from nltk.corpus import stopwords
 ps = PorterStemmer()
-# Load model and vectorizer
-model = pickle.load(open('model2.pkl', 'rb'))
-tfidfvect = pickle.load(open('tfidfvect2.pkl', 'rb'))
+# Load the logistic regression model from the pkl file
+with open('model2.pkl', 'rb') as f:
+    model = pickle.load(f)
 
-app = Flask(__name__)
+# Load the CountVectorizer from the pkl file
+with open('tfidfvect2.pkl', 'rb') as f:
+    vectorizer = pickle.load(f)
 
-
-@app.route('/')
-def index():
-    return "Hello world"
-
-
-@app.route('/predict', methods=['GET','POST'])
-def predict():
+# Define a function to preprocess the input news text
+def preprocess_text(text):
     text = request.form.get('text')
     review = re.sub('[^a-zA-Z]', ' ', str(text))
     review = review.lower()
     review = review.split()
     review = [ps.stem(word) for word in review if not word in stopwords.words('english')]
     review = ' '.join(review)
-    review_vect = tfidfvect.transform([review]).toarray()
-    if model.predict(review_vect) == 0:
-        prediction = 'REAL'
+    review_vect = vectorizer.transform([review]).toarray()
+    prediction = model.predict(review_vect)
+    return prediction
+
+# Initialize the Flask app
+app = Flask(__name__)
+
+# Define an API endpoint for classifying news as real or fake
+@app.route('/classify_news', methods=['POST'])
+def classify_news():
+    # Get the news text from the POST request
+    news = request.form.get('news')
+    pred = preprocess_text(news);
+    if pred[0] == 1:
+        return jsonify({'prediction': '1'})
     else:
-        prediction = 'FAKE'
-    return jsonify({'prediction': prediction})
-    # return prediction
+        return jsonify({'prediction': '0'})
 
-
-
-if __name__ == "__main__":
+# Run the Flask app
+if __name__ == '__main__':
     app.run()
